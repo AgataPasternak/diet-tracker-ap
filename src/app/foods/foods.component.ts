c095490768173ccc19f0239e839cf2b0e09683a1
AgataPasternak <akj.pasternak@gmail.com>
2023-05-26 14:01:50 +0200

    HTTP Request

--- src/app/foods/foods.component.ts
+++ src/app/foods/foods.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+export interface Food {
+  data: [
+    {
+      name: string,
+      id: string,
+      caloriesPer100g: number,
+      weight: number,
+      nutriScore: string,
+      tags: string,
+      photo: string
+    }
+  ],
+  lenght: number
+}
 
 @Component({
   selector: 'app-foods',
   templateUrl: './foods.component.html',
   styleUrls: ['./foods.component.scss']
 })
-export class FoodsComponent {
+export class FoodsComponent implements OnInit {
+  // ??? --> dlaczego nie możemy tego od razu inicjalizować?
+  // foods: Food[] = [];
+  foods: Food;
 
+  constructor(private httpClient: HttpClient) {}
+  
+  ngOnInit(): void {
+    this.httpClient.get<Food>('http://localhost:8080/api/foods/')
+    .subscribe((data) => {
+      this.foods = data;
+      console.log(this.foods);
+    })
+  }
 }
