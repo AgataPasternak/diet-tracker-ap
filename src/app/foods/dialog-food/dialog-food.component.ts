9053b2471001ea1ce76aebb6dc65dc144539509c
AgataPasternak <akj.pasternak@gmail.com>
2023-07-17 17:06:50 +0200

    Mat Sort (Dev)

--- src/app/foods/dialog-food/dialog-food.component.ts
+++ src/app/foods/dialog-food/dialog-food.component.ts
@@ -6,24 +6,21 @@ import { FoodsState } from '../foods.state';
 import { TagsState } from '../tags.state';
 
 @Component({
-  selector: 'app-dialog-food',
   templateUrl: './dialog-food.component.html',
   styleUrls: ['./dialog-food.component.scss']
 })
 export class DialogFoodComponent implements OnInit {
-  inputData: any;
+  inputData: any; // zrobić interfejs; usunąć i używać samej data
 
   private ref = inject(MatDialogRef<DialogFoodComponent>);
   private fb = inject(FormBuilder);
   private state = inject(FoodsState);
-  public data: any = inject(MAT_DIALOG_DATA);
+  public data: any = inject(MAT_DIALOG_DATA); // zrobić interfejs
   public tagsState = inject(TagsState);
 
   postInLoading$ = this.state.postInLoading$;
   responseFood$ = this.state.food$;
   tags$ = this.tagsState.tags$;
-
-
 
   ngOnInit(): void {
     this.inputData = this.data;
@@ -33,24 +30,22 @@ export class DialogFoodComponent implements OnInit {
         this.foodForm.patchValue(data);
       })
     }
+    if (this.inputData.readonly) {
+      this.foodForm.disable();
+    }
     this.tagsState.getTags();
-
   }
 
   foodForm = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
     caloriesPer100g: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-    weight: undefined,
+    weight: [0, [Validators.required, Validators.pattern('^[0-9]*$')]],
     nutriScore: ['', [Validators.required, Validators.pattern('^[A-E]$'), Validators.minLength(1), Validators.maxLength(1)]],
     tags: ['', [Validators.required]],
     photo: ['', [Validators.required]]
   })
 
   get name() { return this.foodForm.get('name'); }
-
-  // searchForm = this.fb.group({
-  //   search: ['']
-  // })
 
   onSubmit() {
     this.state.postFood(this.foodForm.value as Food);
--- src/app/foods/foods.component.ts
+++ src/app/foods/foods.component.ts
@@ -1,11 +1,13 @@
 
-import { AfterViewInit, Component, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 
+import { FormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { DialogFoodComponent } from './dialog-food/dialog-food.component';
+import { Food } from './foods.model';
 import { FoodsState } from './foods.state';
 
 @Component({
@@ -14,44 +16,64 @@ import { FoodsState } from './foods.state';
   styleUrls: ['./foods.component.scss']
 })
 
-export class FoodsComponent implements OnInit, AfterViewInit {
-  // ??? --> dlaczego columnsToDisplay nie musi widzieć object Response
+export class FoodsComponent implements OnInit {
+  @ViewChild(MatSort)
+  sort!: MatSort;
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
   columnsToDisplay = ['id', 'name', 'caloriesPer100g', 'nutriScore', 'actionsColumn'];
 
   private state = inject(FoodsState);
-  public dialog = inject(MatDialog);
+  dialog = inject(MatDialog);
+  private fb = inject(FormBuilder);
 
   response$ = this.state.foods$;
   loading$ = this.state.loading$;
   deleteInProgress$ = this.state.deleteInProgress$;
   postInLoading$ = this.state.postInLoading$;
   errorMessage$ = this.state.errorMessage$;
+  search = this.fb.control('');
 
-  dataSource: MatTableDataSource<Response>;
-
-  @ViewChild(MatSort)
-  sort: MatSort;
-
-  @ViewChild(MatPaginator)
-  paginator: MatPaginator;
+  dataSource: MatTableDataSource<Food>;
 
   ngOnInit(): void {
     this.state.getFoods();
-    // sortowanie - jako dataSource podaje Observable
-    // this.dataSource = new MatTableDataSource(this.response$);
-    // this.dataSource.sort = this.sort;
-
-
-  }
-
-  ngAfterViewInit() {
-    // paginacja - jako dataSource podaje zasubskrybowany object data
     this.response$.subscribe((data) => {
-      // this.dataSource = new MatTableDataSource(data);
-      // this.dataSource.sort = this.sort;
-      // this.dataSource.paginator = this.paginator;
+      console.log(data);
+      // this.foods = data.data;
+      this.dataSource = new MatTableDataSource(data.data);
+      this.dataSource.sort = this.sort;
+
     });
   }
+
+  // ngAfterViewInit() {
+  //   // w ngAfter bo nie mamy nagłówków zanim się nie załaduje
+  //   // this.dataSource = new MatTableDataSource(this.response$);
+  //   // this.dataSource.sort = this.sort;
+
+  //   // paginacja - jako dataSource podaje zasubskrybowany object data
+  //   this.response$.subscribe((data) => {
+
+  //     this.dataSource = new MatTableDataSource(data.data);
+  //     // this.dataSource.sort = this.sort;
+  //     this.dataSource.paginator = this.paginator;
+
+  //   });
+  // }
+
+  // ngAfterViewInit(): void {
+  //   this.tableDataSource.sort = this.sort;
+
+  //   if (!this.paginator) {
+  //     return;
+  //   }
+  //   this.matPaginator = this.paginator.matPaginator;
+  //   this.tableDataSource.paginator = this.matPaginator;
+  //   this.paginator.page.subscribe((page) => {
+  //     this.changePage.emit(page);
+  //   });
 
   onDeleteFood(id: string) {
     this.state.deleteFoods(id);
@@ -64,6 +86,7 @@ export class FoodsComponent implements OnInit, AfterViewInit {
       this.state.getFoods();
     }
   }
+
   addFoodDialog() {
     this.openDialog('Dodaj produkt', true, undefined, false, false);
   }
@@ -73,8 +96,10 @@ export class FoodsComponent implements OnInit, AfterViewInit {
   onEditFood(id: string) {
     this.openDialog('Edytuj produkt', true, id, false, true);
   }
-  openDialog(title: any, showAccions: boolean, id?: string, readonly?: boolean, editMode?: boolean) {
-    var _dialog = this.dialog.open(DialogFoodComponent, {
+  // poprawić literówkę Acctionss
+  // argumnet funckji niech będzie obiektem (w dialogu)
+  openDialog(title: string, showAccions: boolean, id?: string, readonly?: boolean, editMode?: boolean) {
+    const dialog = this.dialog.open(DialogFoodComponent, {
       width: '40%',
       enterAnimationDuration: 300,
       exitAnimationDuration: 300,
@@ -86,9 +111,5 @@ export class FoodsComponent implements OnInit, AfterViewInit {
         editMode: editMode
       }
     });
-    _dialog.afterClosed().subscribe(result => {
-      // console.log(result);
-    }
-    );
   }
 }
